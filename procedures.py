1c08ab92c66b91bd8b843ee7943633424b4dbb77
Alireza Ziaee <alrz1999@gmail.com>
2021-04-27 12:18:46 +0430

    start and terminals added

---
+++ procedures.py
@@ -0,0 +1,3 @@
+start = 'Program'
+terminals = {'$', 'ID', ';', '[', 'NUM', ']', '(', ')', 'int', 'void', ',', '{', '}', 'break', 'if', 'else', 'while',
+             'return', 'for', '=', '<', '==', '+', '-', '*', 'ε'}
