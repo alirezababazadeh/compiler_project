1e0a305557664ee7b5ed2eef650bd4aed8b51676
Alireza Babazadeh <alireza_bz@yahoo.com>
2021-06-04 15:59:12 +0430

    debug some bugs

--- code_generator.py
+++ code_generator.py
@@ -8,32 +8,124 @@ class CodeGenerator:
         self.program_block = []
         self.data_cursor = 1000
         self.temp_cursor = 500
-        self.semantic_routines = {'ptype': self.ptype,
-                                  'pid': self.pid,
-                                  'pop': self.pop,
-                                  'pnum': self.pnum,
-                                  'save_array': self.save_array,
-                                  'save': self.save,
-                                  'jpf_save': self.jpf_save,
-                                  'jp': self.jp,
-                                  'label': self.label,
-                                  'while': self.iter,
-                                  'assign': self.assign,
-                                  'array_index': self.array_index,
-                                  'relop': self.relop,
-                                  'addop': self.addop,
-                                  'mult': self.mult,
-                                  'sub': self.sub,
-                                  'finish': self.finish
-                                  }
 
     def get_temp(self):
         pointer = self.temp_cursor
         self.temp_cursor += 4
         return pointer
 
+    def get_new_data_address(self):
+        self.data_cursor += 4
+        return self.data_cursor
+
     def generate_code(self, action_symbol, *args):
-        self.semantic_routines[action_symbol](*args)
+        if action_symbol == 'ptype':
+            self.semantic_stack.append(args[0])
+
+        elif action_symbol == 'pid':
+            if args[0] != 'finish':
+                lexeme = self.symbol_table.get_lexeme(args[0])
+                address = self.get_new_data_address()
+                if lexeme.type is None:
+                    lexeme.update_lexeme(address, self.semantic_stack.pop())
+                    self.program_block.append(f'(ASSIGN, #0, {address}, )')
+                    self.semantic_stack.append(address)
+                else:
+                    self.semantic_stack.append(lexeme.address)
+
+        elif action_symbol == 'pop':
+            self.semantic_stack.pop()
+
+        elif action_symbol == 'pnum':
+            self.semantic_stack.append('#' + args[0])
+
+        elif action_symbol == 'save_array':
+            count = int(self.semantic_stack.pop()[1:])
+            self.semantic_stack.pop()
+            for i in range(count - 1):
+                address = self.get_new_data_address()
+                self.program_block.append(f'(ASSIGN, #0, {address}, )')
+
+        elif action_symbol == 'save':
+            self.semantic_stack.append(len(self.program_block))
+            self.program_block.append('')
+
+        elif action_symbol == 'jpf_save':
+            index = self.semantic_stack.pop()
+            self.program_block[index] = f'(JPF, {self.semantic_stack.pop()}, {len(self.program_block) + 1}, )'
+            self.semantic_stack.append(len(self.program_block))
+            self.program_block.append('')
+
+        elif action_symbol == 'jp':
+            self.program_block[self.semantic_stack.pop()] = f'(JP, {len(self.program_block)}, , )'
+
+        elif action_symbol == 'label':
+            self.semantic_stack.append(len(self.program_block))
+
+        elif action_symbol == 'while':
+            index = self.semantic_stack.pop()
+            self.program_block[index] = f'(JPF, {self.semantic_stack.pop()}, {len(self.program_block) + 1}, )'
+            self.program_block.append(f'(JP, {self.semantic_stack.pop()}, , )')
+
+        elif action_symbol == 'assign':
+            left = self.semantic_stack.pop()
+            right = self.semantic_stack.pop()
+            self.program_block.append(f'(ASSIGN, {left}, {right}, )')
+            self.semantic_stack.append(right)
+
+        elif action_symbol == 'array_index':
+            temp_address = self.get_temp()
+            index = self.semantic_stack.pop()
+            id = self.semantic_stack.pop()
+
+            self.program_block.append(f'(MULT, {index}, #4, {temp_address})')
+            self.program_block.append(f'(ADD, #{id}, {temp_address}, {temp_address})')
+            self.semantic_stack.append('@' + str(temp_address))
+
+        elif action_symbol == 'relop':
+            op2 = self.semantic_stack.pop()
+            operator = self.semantic_stack.pop()
+            op1 = self.semantic_stack.pop()
+
+            temp_address = self.get_temp()
+            self.semantic_stack.append(temp_address)
+
+            if operator == '<':
+                self.program_block.append(f'(LT, {op1}, {op2}, {temp_address})')
+            elif operator == '==':
+                self.program_block.append(f'(EQ, {op1}, {op2}, {temp_address})')
+
+        elif action_symbol == 'addop':
+            op2 = self.semantic_stack.pop()
+            operator = self.semantic_stack.pop()
+            op1 = self.semantic_stack.pop()
+
+            temp_address = self.get_temp()
+            if operator == '+':
+                self.program_block.append(f'(ADD, {op1}, {op2}, {temp_address})')
+            elif operator == '-':
+                self.program_block.append(f'(SUB, {op1}, {op2}, {temp_address})')
+            self.semantic_stack.append(temp_address)
+
+        elif action_symbol == 'mult':
+            op2 = self.semantic_stack.pop()
+            op1 = self.semantic_stack.pop()
+
+            temp_address = self.get_temp()
+            self.program_block.append(f'(MULT, {op1}, {op2}, {temp_address})')
+            self.semantic_stack.append(temp_address)
+
+        elif action_symbol == 'sub':
+            op = self.semantic_stack.pop()
+            temp_address = self.get_temp()
+
+            self.program_block.append(f'(SUB, #0, {op}, {temp_address})')
+            self.semantic_stack.append(temp_address)
+
+        elif action_symbol == 'finish':
+            address = self.semantic_stack.pop()
+            self.program_block.append(f'(PRINT, {address}, , )')
+            self.semantic_stack.append(None)
 
     def write_to_file(self, address):
         output = ''
@@ -41,115 +133,3 @@ class CodeGenerator:
             output += f'{i}\t{program}\n'
         output = output[:-1]
         open(f'{address}', 'w').write(output)
-
-    def ptype(self, *args):
-        self.semantic_stack.append(args[0])
-
-    def get_new_data_address(self):
-        self.data_cursor += 4
-        return self.data_cursor
-
-    def pid(self, *args):
-        if args[0] != 'finish':
-            lexeme = self.symbol_table.get_lexeme(args[0])
-            address = self.get_new_data_address()
-            if lexeme.type is None:
-                lexeme.update_lexeme(address, self.semantic_stack.pop())
-                self.program_block.append(f'(ASSIGN, #0, {address}, )')
-                self.semantic_stack.append(address)
-            else:
-                self.semantic_stack.append(lexeme.address)
-
-    def pop(self, *args):
-        self.semantic_stack.pop()
-
-    def pnum(self, *args):
-        self.semantic_stack.append('#' + args[0])
-
-    def save_array(self, *args):
-        count = int(self.semantic_stack.pop()[1:])
-        self.pop()
-        for i in range(count - 1):
-            address = self.get_new_data_address()
-            self.program_block.append(f'(ASSIGN, #0, {address}, )')
-
-    def save(self, *args):
-        self.semantic_stack.append(len(self.program_block))
-        self.program_block.append('')
-
-    def jpf_save(self, *args):
-        index = self.semantic_stack.pop()
-        self.program_block[index] = f'(JPF, {self.semantic_stack.pop()}, {len(self.program_block) + 1}, )'
-        self.semantic_stack.append(len(self.program_block))
-        self.program_block.append('')
-
-    def jp(self, *args):
-        self.program_block[self.semantic_stack.pop()] = f'(JP, {len(self.program_block)}, , )'
-
-    def label(self, *args):
-        self.semantic_stack.append(len(self.program_block))
-
-    def iter(self, *args):
-        index = self.semantic_stack.pop()
-        self.program_block[index] = f'(JPF, {self.semantic_stack.pop()}, {len(self.program_block) + 1}, )'
-        self.program_block.append(f'(JP, {self.semantic_stack.pop()}, , )')
-
-    def assign(self, *args):
-        left = self.semantic_stack.pop()
-        right = self.semantic_stack.pop()
-        self.program_block.append(f'(ASSIGN, {left}, {right}, )')
-        self.semantic_stack.append(right)
-
-    def array_index(self, *args):
-        temp_address = self.get_temp()
-        index = self.semantic_stack.pop()
-        id = self.semantic_stack.pop()
-
-        self.program_block.append(f'(MULT, {index}, #4, {temp_address})')
-        self.program_block.append(f'(ADD, #{id}, {temp_address}, {temp_address})')
-        self.semantic_stack.append('@' + str(temp_address))
-
-    def relop(self, *args):
-        op2 = self.semantic_stack.pop()
-        operator = self.semantic_stack.pop()
-        op1 = self.semantic_stack.pop()
-
-        temp_address = self.get_temp()
-        self.semantic_stack.append(temp_address)
-
-        if operator == '<':
-            self.program_block.append(f'(LT, {op1}, {op2}, {temp_address})')
-        elif operator == '==':
-            self.program_block.append(f'(EQ, {op1}, {op2}, {temp_address})')
-
-    def addop(self, args=None):
-        op2 = self.semantic_stack.pop()
-        operator = self.semantic_stack.pop()
-        op1 = self.semantic_stack.pop()
-
-        temp_address = self.get_temp()
-        if operator == '+':
-            self.program_block.append(f'(ADD, {op1}, {op2}, {temp_address})')
-        elif operator == '-':
-            self.program_block.append(f'(SUB, {op1}, {op2}, {temp_address})')
-        self.semantic_stack.append(temp_address)
-
-    def mult(self, *args):
-        op2 = self.semantic_stack.pop()
-        op1 = self.semantic_stack.pop()
-
-        temp_address = self.get_temp()
-        self.program_block.append(f'(MULT, {op1}, {op2}, {temp_address})')
-        self.semantic_stack.append(temp_address)
-
-    def sub(self, *args):
-        op = self.semantic_stack.pop()
-        temp_address = self.get_temp()
-
-        self.program_block.append(f'(SUB, #0, {op}, {temp_address})')
-        self.semantic_stack.append(temp_address)
-
-    def finish(self, *args):
-        address = self.semantic_stack.pop()
-        self.program_block.append(f'(PRINT, {address}, , )')
-        self.semantic_stack.append(None)
--- compiler.py
+++ compiler.py
@@ -17,7 +17,7 @@ class Parser:
         self.procedure_repository.run_procedure(self.procedure_repository.start)
         TreeRenderer(self.procedure_repository.tree_generator.tree).write_to_file('parse_tree.txt')
         self.procedure_repository.error_handler.write_to_file('syntax_errors.txt')
-        self.procedure_repository.code_generator.write_to_file('output.txt')
+        self.procedure_repository.code_generator.write_to_file('PA3_Resources/Tester/output.txt')
 
 
 class ProcedureRepository:
@@ -113,7 +113,7 @@ class ProcedureRepository:
 
 
 output_path = 'tester\\output.txt'
-input_path = 'input.txt'
+input_path = 'PA3_Resources/T5/input.txt'
 
 
 def main():
